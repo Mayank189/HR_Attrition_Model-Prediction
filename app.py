ce7fe6124ced166bc7a27efa3b394a8cf7b956ed
Mayank Jaiswal <78966647+Mayank189@users.noreply.github.com>
2022-01-02 16:23:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template, url_for
+import pickle
+import pandas as pd
+
+
+app = Flask(__name__)
+model = pickle.load(open('xgb_1.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    #return 'Hello World'
+    return render_template('home.html')
+    #return render_template('index.html')
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+
+    #Extract input from form
+    joblevel=request.form.get("JobLevel")
+    stockoption=request.form.get("StockOptionLevel")
+    yearswithmanager=request.form.get("YearsWithCurrManager")
+    monthlyincome=request.form.get("MonthlyIncome")
+    totalworking=request.form.get("TotalWorkingYears")
+    yearssincelast=request.form.get("YearsSinceLastPromotion")
+    yearsincurrentrole=request.form.get("YearsInCurrentRole")
+    yearsatcompany=request.form.get("YearsAtCompany")
+    
+    #Create the Dataframe based on input
+    
+    input_variables=pd.DataFrame([[joblevel,stockoption,yearswithmanager,monthlyincome,totalworking,yearssincelast,yearsincurrentrole,yearsatcompany]],
+                                    columns=['JobLevel','StockOptionLevel','YearsWithCurrManager','MonthlyIncome','TotalWorkingYears','YearsSinceLastPromotion','YearsInCurrentRole','YearsAtCompany'],
+                                    dtype=float,
+                                    index=['input'])
+                                    
+                                    
+    prediction=model.predict(input_variables)[0]
+     
+     
+    
+    return render_template('home.html', prediction_text="Attrition {}".format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080,debug=True)
